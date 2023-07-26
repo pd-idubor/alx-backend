ba1406ac88e177ef20cc9ab2397a89c3e047c63c
Precious Idubor <preciousidubor@gmail.com>
2023-07-26 10:13:55 -0700

    LRU caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""LRU caching"""
+from collections import OrderedDict
+BaseCaching = __import__('0-basic_cache').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU caching system"""
+    def __init__(self):
+        """Initializer"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Assigns to dict the item value for the key"""
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+        elif len(self.cache_data) >= type(self).MAX_ITEMS:
+            lru, val = self.cache_data.popitem(last=False)
+            print("DISCARD: {}".format(lru))
+        if key and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns thr value of cache_data key"""
+        if key and self.cache_data.get(key) is not None:
+            return self.cache_data[key]
