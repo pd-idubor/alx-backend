7b1e4d9ab07614d1ce5070bec1e229f98fa4a4f9
Precious Idubor <preciousidubor@gmail.com>
2023-08-04 15:09:52 -0700

    Module imported

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 from typing import Union
 
