76c53905c80358ac3ea400648ec683bfe1cdc51a
Precious Idubor <preciousidubor@gmail.com>
2023-08-04 23:20:54 -0700

    Users added

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -5,6 +5,14 @@ from flask_babel import Babel
 from typing import Union
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 class Config(object):
     """Config class"""
     LANGUAGES = ["en", "fr"]
