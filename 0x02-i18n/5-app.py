e1420f78e72b656d07f2f1e54b4a2cfe91720d5b
Precious Idubor <preciousidubor@gmail.com>
2023-08-04 21:06:57 -0700

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -5,14 +5,6 @@ from flask_babel import Babel
 from typing import Union
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
 class Config(object):
     """Config class"""
     LANGUAGES = ["en", "fr"]
@@ -23,6 +15,19 @@ class Config(object):
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+def get_user(login_as: int) -> Union[dict, None]:
+    """Returns user dict or None if ID is not found """
+    if login_as and int(login_as) in users:
+        return users[int(login_as)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """'Gets' user and set as global"""
+    g.user = get_user(request.args.get('login_as'))
 
 
 @app.route('/',  strict_slashes=False)
@@ -38,19 +43,6 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(['LANGUAGES'])
-
-
-def get_user(login_as: int) -> Union[dict, None]:
-    """Returns user dict or None if ID is not found or is NULL"""
-    if login_as and int(login_as) in users:
-        return users[int(login_as)]
-    return None
-
-
-@app.before_request
-def before_request():
-    """Find a user and set it as a global on flask.g.user"""
-    g.user = get_user(request.args.get('login_as'))
 
 
 if __name__ == '__main__':
