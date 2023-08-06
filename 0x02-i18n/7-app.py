2e5099ec2650d64ba6267b6832c1d69fd46c3727
Precious Idubor <preciousidubor@gmail.com>
2023-08-06 14:14:33 -0700

    Inferrung appropraite timezone

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union
+import pytz
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user(login_as: int) -> Union[dict, None]:
+    """Returns user dict or None if ID is not found """
+    if login_as and int(login_as) in users:
+        return users[int(login_as)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """'Gets' user and set as global"""
+    g.user = get_user(request.args.get('login_as'))
+
+
+@app.route('/',  strict_slashes=False)
+def index():
+    """Welcome pg"""
+    return render_template('7-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Language translation selection"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
+
+    h_locale = request.headers.get('locale')
+    if h_locale and h_locale in app.config['LANGUAGES']:
+        return h_locale
+    return request.accept_languages.best_match(['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """Infer appropriate time zone"""
+    try:
+        if request.args.get('timezone'):
+            timezone = request.args.get('timezone')
+            pytz.timezone(timezone)
+            return timezone
+        if g.user:
+            timezone = g.user.get('timezone')
+            pytz.timezone(timezone)
+            return timezone
+    except pytz.exceptions.UnknownTimeZoneError:
+        pass
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+if __name__ == '__main__':
+    app.run()
